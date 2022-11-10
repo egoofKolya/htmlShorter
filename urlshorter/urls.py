20e5d263706caaba27dc609a65d266c48399ad2c
egoofKolya <mykola22dubyna@gmail.com>
2022-11-10 18:52:35 +0200

    setup views

--- urlshorter/urls.py
+++ urlshorter/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('', views.home, name='home'),
     path('short_id/', views.short_id, name='short_id'),
     path('urlshorter/', views.urlshorter, name='urlshorter'),
 ]
